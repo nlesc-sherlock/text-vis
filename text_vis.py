97437c26cabdcd49ecad821650d67f8c49eb3d7d
Willem van Hage <wrvhage@eslt0043.local>
2015-12-03 15:31:52 +0100

    generate multiple wordclouds, one per input file

--- text_vis.py
+++ text_vis.py
@@ -7,14 +7,18 @@ import os
 from os.path import join, getsize
 import sys
 
-text = ""
+corpus_text = ""
 for root, dirs, files in os.walk(sys.argv[1]):
   for fn in files:
     with open(os.path.join(root, fn),"r") as f:
+      text = ""
       for l in f.readlines():
         text = text + l
+      wordcloud = WordCloud().generate(text)
+      wordcloud = WordCloud(max_font_size=40, relative_scaling=.5).generate(text)
+      wordcloud.to_file(os.path.join(sys.argv[2], fn + "-wordcloud.png"))
+      corpus_text = corpus_text + text
 
-wordcloud = WordCloud().generate(text)
-
-wordcloud = WordCloud(max_font_size=40, relative_scaling=.5).generate(text)
-wordcloud.to_file(os.path.join(sys.argv[2], "wordcloud.png"))
+wordcloud = WordCloud().generate(corpus_text)
+wordcloud = WordCloud(max_font_size=40, relative_scaling=.5).generate(corpus_text)
+wordcloud.to_file(os.path.join(sys.argv[2], "ALL-wordcloud.png"))
